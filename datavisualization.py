37ef689a5d632ea6dfa2c79455499dd390afe5ce
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-05 15:51:48 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -4,34 +4,55 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from extract_data import load_data
+import pandas as pd
+import plotly.express as px
+# from IPython.display import Image
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.figure_factory as ff
+import plotly.io as pio
+import plotly.graph_objects as go
+import io
+from PIL import Image
+a =[]
+categ = []
+numer = []
+def visualise_data():  
+    dataset = data_preprocess()
+    print("dataset------------", dataset)
+    col=list(dataset.columns)
+    col.remove("y")
+    print(col)
 
-def visualise_data():
-    df = load_data()
-    categorical_features, numerical_features = data_preprocess()
-    for categorical_feature in categorical_features:
-        fig, axs = plt.subplots(figsize=(5,5))
-        sns.countplot(y=categorical_feature,data=df)
-        plt.xlabel(categorical_feature)
-        plt.title(categorical_feature)
-        plt.show()
-    for categorical_feature in categorical_features:
-        sns.catplot(x='y', col=categorical_feature, kind='count', data=df)
-        plt.show()
-    for numerical_feature in numerical_features:
-        fig, axs = plt.subplots(figsize=(5,4))
-        sns.distplot(df[numerical_feature])
-        plt.xlabel(numerical_feature)
-        plt.show()
-    for numerical_feature in numerical_features:
-        fig, axs = plt.subplots(figsize=(5,4))
-        sns.boxplot(df[numerical_feature])
-        plt.xlabel(numerical_feature)
-        plt.show()
-    data_num = df[numerical_features]
-    cor_mat = data_num.corr()
-    plt.figure(figsize = (12,12))
-    sns.heatmap(cor_mat,annot=True)
-    plt.show()
-    return df
+    for i in col:
+        fig = px.box(dataset, y=i)
+        fig.update_layout(template='plotly_dark')
+        #fig.update_layout(plot_bgcolor = "plotly_dark")
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        # fig.show()
+        fig.write_image(f"{i}.jpg")
+        # a.append(fig)
+    for i in col:
+        fig = px.histogram(dataset, y=i, marginal="box")
+        fig.update_layout(template='plotly_dark')
+        fig.update_xaxes(showgrid=False, zeroline=False)
+        fig.update_yaxes(showgrid=False, zeroline=False)
+        # fig.show()
+        fig.write_image(f"{i}_hist.jpg")
+        # a.append(fig)
+
+    df=dataset.drop("y",axis=1)
+    y=df.corr().columns.tolist()
+    z=df.corr().values.tolist()
+    z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
+    fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
+    fig.update_layout(template='plotly_dark')
+    # fig.show()
+    fig.write_image("img.jpg")
+    return dataset
 
 visualise_data()
